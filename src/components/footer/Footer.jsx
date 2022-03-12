6ad9d8af6cd53802a63b6792b19a7aca200e2177
Zhakshylyk <zhakshylykkozhobekov@gmail.com>
2022-03-12 12:13:10 +0600

    all

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,14 +3,46 @@ import logo from '../../assets/img/istore_logo.png'
 import styles from './Footer.module.scss'
 
 function Footer() {
-    return(
+    return (
         <div className={styles.footer}>
-            <div>
-                <img src={logo} alt=""/>
+            <div className={styles.block}>
+                <img src={logo} alt="footerLogo" />
+                <p>Магазин техники Apple в Бишкеке</p>
+                <h5>Режим работы</h5>
+                <p>Пн - Вс 10:00 - 20:00</p>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={styles.products}>
+                <h5>Продукция</h5>
+                <a href="/">
+                    Mac
+                </a>
+                <a href="/">
+                    iPhone
+                </a>
+                <a href="/">
+                    Watch
+                </a>
+                <a href="/">
+                    iPad
+                </a>
+            </div>
+            <div className={styles.contacts}>
+                <h5>
+                    Контакты
+                </h5>
+                <p>
+                    пр. Манаса, 40 (пер. ул. Киевская)
+                </p>
+                <a href="/">
+                    +996 (555) 802 000 +996
+                </a>
+                <a href="/">
+                    +996 (505) 802 000
+                </a>
+                <a href="/">
+                    istorekg@gmail.com
+                </a>
+            </div>
         </div>
     )
 }
--- src/pages/basket/Basket.jsx
+++ src/pages/basket/Basket.jsx
@@ -1,24 +1,61 @@
 import React from 'react'
-import {useSelector, useDispatch} from "react-redux";
-import {removeInCard} from "../../redux/actions";
+import { useSelector, useDispatch } from "react-redux";
+import { removeInCard } from "../../redux/actions";
 import styles from './Basket.module.scss'
-import {BasketEmpty} from "../../components";
+import { BasketEmpty } from "../../components";
+import { FaTimes } from 'react-icons/fa'
 
 function Basket() {
     const dispatch = useDispatch()
     const state = useSelector(state => state.addToCard)
     return (
         <div className={styles.basket}>
-            {state.length ===  0 ? <BasketEmpty/> : state.map((item) => {
-                return (
-                    <div key={item.id}>
-                        <h2>
-                            {item.name}
-                        </h2>
-                        <button onClick={() => dispatch(removeInCard(item))}>Remove</button>
-                    </div>
-                )
-            })
+            {state.length === 0 ? <BasketEmpty /> : <div>
+                <div className={styles.title}>
+                    <h1>Корзина</h1>
+                    <p>Товары в вашей корзине</p>
+                </div>
+                {state.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <div className={styles.productName}>
+                                <div className={styles.product}>
+                                    <h3>Продукт:</h3>
+                                    <p>
+                                        {item.name}
+                                    </p>
+                                    <div className={styles.detail}>
+                                        <img src={item.image}
+                                            alt="imgProduct" />
+                                        <div>
+                                            <div className={styles.price}>
+                                                <h4>Цена:</h4>
+                                                <p>{item.price}</p>
+                                            </div>
+                                            <p>
+                                                {item.display}
+                                            </p>
+                                            <p>
+                                                {item.color}
+                                            </p>
+                                        </div>
+
+
+                                    </div>
+
+                                </div>
+                                <FaTimes
+                                    onClick={() => dispatch(removeInCard(item))}
+                                    className={styles.remove}
+                                    title="Удалить из корзины"
+                                >
+                                    Remove
+                                </FaTimes>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
             }
         </div>
     )
